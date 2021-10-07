1c7a329ee89f69e0a4099ca529289b18b4f76576
Iuri Silva <iuricold99@gmail.com>
2021-10-07 17:07:19 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,15 @@
-//Dark mode
-const alterarTema = document.querySelector(
-  '.mudar-tema input[type="checkbox"]'
-);
-  
-const temaAtual = localStorage.getItem("tema");
+document.querySelector("button").addEventListener("click", function (event) {
+  var text_to_copy = document.getElementById("chave").innerHTML;
 
-if (temaAtual) {
-  document.documentElement.setAttribute("data-tema", temaAtual);
-  if (temaAtual === "dark") {
-    alterarTema.checked = true;
+  if (!navigator.clipboard) {
+  } else {
+    navigator.clipboard
+      .writeText(text_to_copy)
+      .then(function () {
+        document.querySelector("button").innerText = "Copiado!";
+      })
+      .catch(function () {
+        document.querySelector("button").innerText = "Ops, ocorreu um erro! :(";
+      });
   }
-}
-  
-function mudarTema(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-tema", "dark");
-    localStorage.setItem("tema", "dark");
-  }else {
-    document.documentElement.setAttribute("data-tema", "light");
-    localStorage.setItem("tema", "light");
-  }
-}
-  
-alterarTema.addEventListener("change", mudarTema, false);
-  
-//Cópiar chave PIX
-document.querySelector("button").addEventListener("click",
-  function (event) {
-    var textRange = document.createRange()
-    textRange.selectNode(document.getElementById("chave"))
-    window.getSelection().removeAllRanges()
-    window.getSelection().addRange(textRange)
-    document.execCommand("copy")
-    window.getSelection().removeAllRanges()
-    document.querySelector("button").innerText = 'Copiado!'
-})
+});
