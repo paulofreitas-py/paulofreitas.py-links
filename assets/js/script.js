6d6e55aa0e234c1526c559759d0974113dc3003e
iuricode <iuricold99@gmail.com>
2023-10-07 10:30:24 -0300

    tirando o telegram

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("button").addEventListener("click", function () {
-  var textRange = document.createRange();
-  textRange.selectNode(document.getElementById("chave"));
-  window.getSelection().removeAllRanges();
-  window.getSelection().addRange(textRange);
-  document.execCommand("copy");
-  window.getSelection().removeAllRanges();
-  document.querySelector("button").innerText = "Copiado!";
-});
